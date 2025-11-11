134a7d2ec9f01116b65c3fcfb669445e07ed8f23
chrisTran110 <156842017+chrisTran110@users.noreply.github.com>
2025-11-11 14:37:18 -0700

    Delete Test.py

--- Test.py
+++
@@ -1,5 +0,0 @@
-x = int(input())
-if x < 50:
-  print("You are under 50") # If x is less than 50, print the message. 
-else:
-  print("You are over 50") # If x is more than 50, ignore the print statement above and print this message instead. 
